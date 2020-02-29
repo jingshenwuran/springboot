e69fa079a7dd8dee76cb0e0dde416cab58e5d842
Administrator <Administrator@WIN-LLEBOUOSG5L>
2020-02-29 16:13:56 +0800

    123

---
+++ src/main/java/com/woniuxy/te/SpringBoot000000Application.java
@@ -0,0 +1,13 @@
+package com.woniuxy.te;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot000000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot000000Application.class, args);
+	}
+
+}
